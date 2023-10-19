f5891a0314b668ddc1c455590ae9c535e0a66089
Eric Allam <eallam@icloud.com>
2023-10-19 17:47:07 +0100

    Go back to the old topic key

--- src/helpers/novu.ts
+++ src/helpers/novu.ts
@@ -4,13 +4,5 @@ import { Repositories } from "@prisma/client";
 export const novu = new Novu(process.env.NOVU_SECRET!);
 
 export function repoTopic(repo: Repositories) {
-  // Convert the repository URL to a topic with the format:
-  // From https://github.com/triggerdotdev/trigger.dev
-  // to triggerdotdev-trigger.dev
-
-  const url = new URL(repo.url);
-  const path = url.pathname.split("/").filter(Boolean);
-  const topic = path.join("-");
-
-  return `repository:${topic}`;
+  return `repository:${repo.id}`;
 }
